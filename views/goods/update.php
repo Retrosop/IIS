4d9f3af51f898a6d2089808819e68c77ddccd31b
anatolit79 <126828766+anatolit79@users.noreply.github.com>
2023-03-03 10:42:20 +1000

    Create first page

---
+++ views/goods/update.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+/** @var yii\web\View $this */
+/** @var app\models\Goods $model */
+
+$this->title = 'Update Goods: ' . $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Goods', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'idgoods' => $model->idgoods]];
+$this->params['breadcrumbs'][] = 'Update';
+?>
+<div class="goods-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
