872fb802893633e255b51df35a388004b4c7f637
eregnwald <106878917+eregnwald@users.noreply.github.com>
2023-06-12 19:55:15 +1000

    Add files via upload

---
+++ controllers/SiteController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class SiteController extends Controller
+{
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+	public function actionOnas()
+	{
+		return $this->render('onas');
+	}
+	public function actionCart()
+{
+		return $this->render('cart');
+   
+}
+}
