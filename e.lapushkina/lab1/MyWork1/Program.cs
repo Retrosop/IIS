b89ae9cab8f7160db69f4a7e313e74a72021f273
elenalapushkina <149218648+elenalapushkina@users.noreply.github.com>
2024-02-10 12:16:59 +1000

    выполнена 1 половина лаб 1

--- e.lapushkina/lab1/MyWork1/Program.cs
+++ e.lapushkina/lab1/MyWork1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LR_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа №1");
+            Console.WriteLine("");
+            Console.WriteLine("Выполнил: Лапушкина Елена Павловна");
+            Console.WriteLine("группа: ИиСит 1721");
+            Console.WriteLine("Наименование лабораторной работы: Ис");
+            Console.WriteLine("");
+            Console.WriteLine("Для завершения работы программы нажмите любую клавишу...");
+
+           
+        }
+    }
+}
