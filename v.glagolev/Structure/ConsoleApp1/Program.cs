e6adc067cd0b28d71860fc79d700532fb152a8a2
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-02-17 10:35:28 +1000

    Learn Structure

---
+++ v.glagolev/Structure/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+	static void Main(string[] args)
+	{
+        Console.WriteLine("Hello World");
+		Person tom1 = new Person();
+		tom1.Print();
+		Person tom2 = new Person("Tom");
+		tom2.Print();
+		Person tom3 = new Person("Tom", 30);
+		tom3.Print();
+
+		Person2 tom4 = new Person2("Tom");
+		tom4.Print();
+
+		decimal a = 3.2m;
+    }
+}
+struct Person
+{
+	public string name;
+	public int age;
+
+	public Person() : this("Tom")
+	{
+
+	}
+	public Person(string name): this(name, 1)
+	{
+	}
+
+	public Person(string name, int age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+	public void Print() => Console.WriteLine($"Имя {name} Возраст: {age}");
+}
+
+struct Person2 (string name)
+{
+	public string name2 => name + "test";
+	public void Print() => Console.WriteLine($"Имя {name2}");
+}
