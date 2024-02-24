bf9053c7ddee858d6b57fb530070d4bef64d0653
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-02-24 13:15:22 +1000

    Типы данных и ЛР 3

---
+++ v.glagolev/Typedata/ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ObjectiveC;
+
+class Myprogram
+{
+	static void Main(string[] args)
+	{
+        Console.WriteLine("HW");
+		var myPerson = new Person("Ivan", 34);
+		myPerson.Age = 10;
+        Console.WriteLine(myPerson.Age);
+		myPerson.InfoPerson("Банк ООО Сбербанк");
+    }
+}
+
+//private
+//protected
+//internal
+public class MyTypeData
+{
+	int aInt = 100;
+	decimal bDecimal = 1.2m;
+	float cFloat = 1.3382388382323f;
+	bool dBool = false;
+	char eChar = 'a';
+	enum DayTime
+	{
+		Morning,
+		Afternoon,
+		Evening,
+		Night
+	};
+	//struct gStruct;
+
+	//Ссылочные типиы
+	//object myobj;
+	string str;
+	//class nClass;
+	//interface rInterface;
+	//delegate kDelegate;
+
+}
+
+public class Person
+{
+	string name;
+	private int age;
+
+	public int Age
+	{
+		set
+		{
+			if (value < 1 || value > 120)
+				Console.WriteLine("Возраст должен быть валиден");
+			else
+				this.age = value;
+		}
+		get
+		{
+			return this.age;
+		}
+	}
+    public Person(string n, int a)
+    {
+		this.name = n;
+		this.age = a;
+    }
+	public void InfoPerson(string infoTo)
+	{
+        Console.WriteLine($"Сформирован для {infoTo} Персона {this.name} {this.age}");
+    }
+	public void InfoPerson(string infoTo, int infoPeriod)
+	{
+		Console.WriteLine($"Сформирован для {infoTo} Персона {this.name} {this.age} Период {infoPeriod}");
+	}
+}
---
+++ v.glagolev/lab3/Lab3/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+namespace Test;
+class Program
+{
+	static void Main()
+	{
+		TextWriter save_out = Console.Out;
+		TextReader save_in = Console.In;
+		//var myPath = Directory.GetCurrentDirectory();
+		var new_out = new StreamWriter(@"output.txt");
+		var new_in = new StreamReader(@"input.txt");
+		Console.SetOut(new_out);
+		Console.SetIn(new_in);
+		int t, N;
+		double X, Y, Z = 0;
+		t = Convert.ToInt32(Console.ReadLine());
+		N = Convert.ToInt32(Console.ReadLine());
+		X = Convert.ToDouble(Console.ReadLine());
+		Y = Convert.ToDouble(Console.ReadLine());
+		int i, step;
+		double znam = 1, chisl;
+		if (t == 0)
+		{
+			for (i = 1; i <= N; i++)
+			{
+				step = i * 2;
+				znam *= (step - 1) * step;
+				if (i % 2 == 0)
+					chisl = -Math.Pow(Y, step);
+				else
+					chisl = Math.Pow(X, step);
+				Z += (step - 1) * (step + 1) * chisl / znam;
+			}
+		}
+
+		if (t == 1)
+		{
+			i = 1;
+			while (i <= N)
+			{
+				step = i * 2;
+				znam *= (step - 1) * step;
+				if (i % 2 == 0)
+					chisl = -Math.Pow(Y, step);
+				else
+					chisl = Math.Pow(X, step);
+				Z += (step - 1) * (step + 1) * chisl / znam;
+				i++;
+			}
+		}
+		if (t == 2)
+		{
+			i = 1;
+			do
+			{
+				step = i * 2;
+				znam *= (step - 1) * step;
+				if (i % 2 == 0)
+					chisl = -Math.Pow(Y, step);
+				else
+					chisl = Math.Pow(X, step);
+				Z += (step - 1) * (step + 1) * chisl / znam;
+				i++;
+			} while (i <= N);
+
+		}
+		Console.WriteLine(String.Format("{0:0.0000000}", Convert.ToDecimal(Z)));
+		Console.SetOut(save_out); new_out.Close();
+		Console.SetIn(save_in); new_in.Close();
+	}
+}
