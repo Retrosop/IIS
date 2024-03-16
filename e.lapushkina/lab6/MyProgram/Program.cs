edbbd23ebc88a134b31bff86e69d851becd3b53d
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-03-16 13:26:07 +1000

    LAB - 6 Create class Shar

---
+++ e.lapushkina/lab6/MyProgram/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Myprogram
+{
+	class MyProgram
+	{
+		public static void Main()
+		{
+			int radius = - 14;
+			Shar myshar = new Shar();
+			myshar.Radius = radius;
+			Console.WriteLine($"Площадь шара c радиусом {radius} равна {myshar.getPloshad()}");
+			Console.WriteLine($"Диаметр шара c радиусом {radius} равна {myshar.getDiametr()}");
+			Console.WriteLine($"Объем шара c радиусом {radius} равна {myshar.getObiem()}");
+		}
+	}
+}
