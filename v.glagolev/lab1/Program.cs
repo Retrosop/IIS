e5e5fbb90b75abdcbc9fba2bf390c944a7ea2f01
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-03-22 13:22:52 +1000

    LAB - Example lab1

---
+++ v.glagolev/lab1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace LR_One
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a;
+			int b = 7;
+			string str = "Hello? World !!!";
+			a = 123;
+
+            Console.WriteLine("Значение переменной а равно {0}", a);
+			Console.WriteLine("Значение переменной а равно {0}", b);
+			Console.WriteLine("Значение а+b = {0} + {1} = {2}", a,b,a+b);
+            Console.WriteLine(str);
+
+			double g = 1;
+			double h = 2;
+			double d = 3;
+
+			double x = (g * h / d * 17) + (d * 17 / h) - (g + d * 17 + h) / 4;
+			Console.WriteLine("Значение числа x = " + x.ToString());
+			
+ 			Console.ReadKey();
+
+        }
+	}
+}
